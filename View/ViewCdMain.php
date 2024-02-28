5561c9b2a416cf1087aae36d045a82f486ac91e5
Emeline <emeline.delarue31@gmail.com>
2024-02-28 11:10:56 +0100

    Changement CSS page d'accueil

--- View/ViewCdMain.php
+++ View/ViewCdMain.php
@@ -18,32 +18,17 @@ include "../Controller/ControllerRechercheNbr.php"?>
     <link rel="stylesheet" type="text/css" href="../asserts/css/Cloche.css">
     <link rel="icon" href="../asserts/img/logo.png" type="image/x-icon">
 
-    <script src="../asserts/js/AdminMain.js"></script>
     <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
 
     <script src="../asserts/js/script.js"></script>
-
+    <script src="../asserts/js/AdminMain.js"></script>
 
     <script>
-        // Écouteur d'événements pour le bouton d'ouverture
-        document.addEventListener('DOMContentLoaded', function () {
-            document.getElementById('AjEtu').addEventListener('click', function () {
-                openPopup('popUpEtu');
-            });
-        });
-
         document.addEventListener('DOMContentLoaded', function () {
             document.getElementById('AjOffre').addEventListener('click', function () {
                 openPopup('popUpOffre');
             });
         });
-
-        document.addEventListener('DOMContentLoaded', function () {
-            document.getElementById('AjPerso').addEventListener('click', function () {
-                openPopup3();
-            });
-        });
-
     </script>
 </head>
 <body class="body">
