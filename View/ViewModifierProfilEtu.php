9f0c4614f44ed0ac9841bfaf8c859f7846acc4b0
Emeline <emeline.delarue31@gmail.com>
2024-03-19 10:41:05 +0100

    Ajout des messages d'erreurs pour la page d'inscription

--- View/ViewModifierProfilEtu.php
+++ View/ViewModifierProfilEtu.php
@@ -16,15 +16,83 @@
 </head>
 
 <body>
-<?php include("ViewHeader.php"); ?>
+<header class="header">
+    <div class="logo-container">
+        <img src="../asserts/img/logo.png" class="logo">
+    </div>
+
+    <div class="menu-container">
+        <nav>
+            <form method="post" action="../Controller/Connexion/ControllerBtnDeco.php">
+                <ul class="vertical-menu">
+                    <li>
+                        <button type="button" onclick="window.location.href ='ViewAdminMain.php'" name="accueil" value="Accueil" class="btnCreation">  Accueil </button>
+                    </li>
+                    <li>
+                        <button type="button" onclick="window.location.href ='ViewAdminEtu.php'" name="etudiant" value="Etudiant" class="btnCreation"> Etudiant </button>
+                    </li>
+                    <li>
+                        <button type="button" onclick="window.location.href ='ViewAdminEntreprise.php'" name="entreprise" value="Entreprise" class="btnCreation"> Entreprise </button>
+                    </li>
+                    <li>
+                        <button type="button" onclick="window.location.href ='ViewAdminAdministration.php'" name="adminitrsation" class="btnCreation"> Administration </button>
+                    </li>
+                    <li>
+                        <a href="../english/View/ViewAdminMainEn.php"> <img src="../asserts/img/traduction.png" alt="Icone de traduction" class="traduction" id="trad"></a>
+                    </li>
+                    <li id="account-photo">
+                        <img id="photo" src="../asserts/img/utilisateur.png" alt="Image de l'utilisateur" class="utilisateur">
+                        <div id="account-dropdown">
+                            <form method="post" action="../Controller/Connexion/ControllerBtnDeco.php">
+                                <input class="" name="compte" type="submit" value="Mon compte">
+                                <input class="" name="deco" type="submit" value="Se déconnecter">
+                            </form>
+
+                        </div>
+                    </li>
+                    <li>
+                        <div class="notification">
+                            <div class="icon-bell" onclick="toggleNotifications()">
+                                <span class="badge" id="notificationBadge"> </span>
+                            </div>
+                        </div>
+                        <div class="burger-menu" id="burgerMenu" style="display: none;">
+                            <button type="button" id="validationButton" class="validationButton" onclick="fermerNotifications()">Fermer</button>
+
+                            <div class="millieu">
+                                <button type="button" id="showUnreadButton">Notifications non lues</button>
+                                <button type="button" id="showReadButton">Notifications lues</button>
+                            </div>
+
+                            <div>
+                                <h2 id="hnonlu">Notifications non lues</h2>
+                                <ul id="unreadNotificationList" ></ul>
+
+                            </div>
+                            <div>
+                                <h2 id="hlu">Notifications lues</h2>
+                                <ul id="readNotificationList"></ul>
+                            </div>
+
+                            <button type="button" id="validationButton" class="validationButton" ">Valider</button>
+
+                        </div>
+                    </li>
+                </ul>
+            </form>
+        </nav>
+    </div>
+
+</header>
+
 
 <div class="content">
 
     <button type="submit" name="modifier_nom" value="Modifier" class="transparent-button">
-        <img class="editable" id="editIcon" src="../../asserts/img/editer.png" alt="edit">
+        <img class="editable" id="editIcon" src="../asserts/img/editer.png" alt="edit">
     </button>
     <button type="submit" name="annuler_nom" value="Annuler" class="transparent-button">
-        <img class="editable" id="annuleIcon" src="../../asserts/img/annulerModif.png" alt="edit">
+        <img class="editable" id="annuleIcon" src="../asserts/img/annulerModif.png" alt="edit">
     </button>
 
 
--- View/ViewNouvCompte.php
+++ View/ViewNouvCompte.php
@@ -11,37 +11,130 @@
 </head>
 <body class="body">
 
-<button onclick="retourPage()" class="btnRetour">Retour</button>
-
-
 <script>
     function retourPage() {
         window.history.back();
+    }
+
+    document.addEventListener("DOMContentLoaded", function() {
+        var bouton = document.getElementById("valider");
+        bouton.addEventListener("click", validerFormulaire);
+    });
+
+    function validerFormulaire(event) {
+        event.preventDefault();
+
+        var nom = document.querySelector('.input-nom').value;
+        var prenom = document.querySelector('.input-prenom').value;
+        var email = document.querySelector('.input-mail').value;
+        var mdp = document.querySelector('.input-mdp').value;
+        var adresse = document.querySelector('.input-adresse').value;
+        var ville = document.querySelector('.input-ville').value;
+        var cp = document.querySelector('.input-cp').value;
+        var date = document.querySelector('.input-date').value;
+        var anneeetude = document.querySelector('.input-anneeetude').value;
+        var ine = document.querySelector('.input-ine').value;
+
+        var erreur = false;
+
+        if (nom === '') {
+            document.querySelector('.erreur-nom').innerText = 'Veuillez entrer un nom.';
+            erreur = true;
+        } else {
+            document.querySelector('.erreur-nom').innerText = '';
+        }
+
+        if (prenom === '') {
+            document.querySelector('.erreur-prenom').innerText = 'Veuillez entrer un prénom.';
+            erreur = true;
+        } else {
+            document.querySelector('.erreur-prenom').innerText = '';
+        }
+
+        if (email === '') {
+            document.querySelector('.erreur-email').innerText = 'Veuillez entrer une adresse email.';
+            erreur = true;
+        } else {
+            document.querySelector('.erreur-email').innerText = '';
+        }
+
+        if (mdp === '') {
+            document.querySelector('.erreur-mdp').innerText = 'Veuillez entrer un mot de passe.';
+            erreur = true;
+        } else {
+            document.querySelector('.erreur-mdp').innerText = '';
+        }
+
+        if (adresse === '') {
+            document.querySelector('.erreur-adresse').innerText = 'Veuillez entrer une adresse.';
+            erreur = true;
+        } else {
+            document.querySelector('.erreur-adresse').innerText = '';
+        }
+
+        if (ville === '') {
+            document.querySelector('.erreur-ville').innerText = 'Veuillez entrer une ville.';
+            erreur = true;
+        } else {
+            document.querySelector('.erreur-ville').innerText = '';
+        }
+
+        if (cp === '') {
+            document.querySelector('.erreur-cp').innerText = 'Veuillez entrer un code postal.';
+            erreur = true;
+        } else {
+            document.querySelector('.erreur-cp').innerText = '';
+        }
+
+        if (date === '') {
+            document.querySelector('.erreur-date').innerText = 'Veuillez entrer une date de naissance.';
+            erreur = true;
+        } else {
+            document.querySelector('.erreur-date').innerText = '';
+        }
+
+        if (anneeetude === '') {
+            document.querySelector('.erreur-anneeetude').innerText = 'Veuillez entrer une année d étude.';
+            erreur = true;
+        } else {
+            document.querySelector('.erreur-anneeetude').innerText = '';
+        }
+
+        if (ine === '') {
+            document.querySelector('.erreur-ine').innerText = 'Veuillez entrer un INE.';
+            erreur = true;
+        } else {
+            document.querySelector('.erreur-ine').innerText = '';
+        }
+
+        if (!erreur) {
+            event.target.closest("form").submit();
+        }
     }
 </script>
 
 <header class="header">
 
+    <button onclick="retourPage()" class="btnRetour">Retour</button>
+
     <div class="princi-rectangle">
 
         <form action="../Controller/Etudiant/ControllerInscriptionEtu.php" method="post">
-            <div class="nom-rectangle">
+            <div class="nom-rectangle rectangle">
 
-                <input type="text" name="nom" class="input-nom" placeholder="NOM">
-
+                <input type="text" name="nom" class="input-nom input" placeholder="NOM">
+                <span class="erreur-nom erreur-message"></span>
             </div>
 
-            <div class="prenom-rectangle">
+            <div class="prenom-rectangle rectangle">
 
-
-                <input type="text" name="prenom" class="input-prenom" placeholder="Prenom">
-
+                <input type="text" name="prenom" class="input-prenom input" placeholder="Prenom">
+                <span class="erreur-prenom erreur-message"></span>
             </div>
 
-            <div class="formation-rectangle">
+            <div class="formation-rectangle rectangle">
 
-                <label for="formation-select"></label>
-                <select id="formation-select" name="formation">
+                <select class="input" id="formation-select" name="formation">
                     <option value="GEII">GEII</option>
                     <option value="GIM">GIM</option>
                     <option value="GMP">GMP</option>
@@ -55,71 +148,60 @@
 
             </div>
 
-            <div class="mail-rectangle">
+            <div class="mail-rectangle rectangle">
 
-                <input type="email" name="email" class="input-mail" placeholder="Email">
-
+                <input type="email" name="email" class="input-mail input" placeholder="Email">
+                <span class="erreur-email erreur-message"></span>
             </div>
 
-            <div class="mdp-rectangle">
+            <div class="mdp-rectangle rectangle">
 
-                <input type="password" name="mdp" class="input-mdp" placeholder="Mot de Passe">
+                <input type="password" name="mdp" class="input-mdp input" placeholder="Mot de Passe">
+                <span class="erreur-mdp erreur-message"></span>
             </div>
 
-            <div class="adresse-rectangle">
+            <div class="adresse-rectangle rectangle">
 
-                <input type="text" name="adresse" class="input-adresse" placeholder="Adresse">
+                <input type="text" name="adresse" class="input-adresse input" placeholder="Adresse">
+                <span class="erreur-adresse erreur-message"></span>
             </div>
 
-            <div class="ville-rectangle">
+            <div class="ville-rectangle rectangle">
 
-                <input type="text" name="ville" class="input-ville" placeholder="Ville">
+                <input type="text" name="ville" class="input-ville input" placeholder="Ville">
+                <span class="erreur-ville erreur-message"></span>
             </div>
 
-            <div class="cp-rectangle">
+            <div class="cp-rectangle rectangle">
 
-                <input type="text" name="cp" class="input-cp" placeholder="Code Postal">
+                <input type="text" name="cp" class="input-cp input" placeholder="Code Postal">
+                <span class="erreur-cp erreur-message"></span>
             </div>
 
-            <div class="date-rectangle">
-                <input type="date" name="date" class="input-date" placeholder="Date de naissance">
+            <div class="date-rectangle rectangle">
+                <input type="date" name="date" class="input-date input" placeholder="Date de naissance">
+                <span class="erreur-date erreur-message"></span>
             </div>
 
-            <div class="anneeetude-rectangle">
+            <div class="anneeetude-rectangle rectangle">
 
-                <input type="number" name="anneeetude" class="input-anneeetude" placeholder="Année d'étude">
+                <input type="number" name="anneeetude" class="input-anneeetude input" placeholder="Année d'étude">
+                <span class="erreur-anneeetude erreur-message"></span>
             </div>
 
-            <div class="ine-rectangle">
+            <div class="ine-rectangle rectangle">
 
-                <input type="text" name="ine" class="input-ine" placeholder="INE">
+                <input type="text" name="ine" class="input-ine input" placeholder="INE">
+                <span class="erreur-ine erreur-message"></span>
             </div>
 
             <div>
-                <input type="submit" name="valider" value="Création" class="btnCreation">
+                <input type="submit" name="valider" id="valider" value="Création" class="btnCreation">
             </div>
-
-
-
-
         </form>
-
     </div>
-
     <p class="info">Si vous possédez déjà un compte veuillez vous connecter en appuyant ici :</p>
     <a href="ViewConnexion.html" class="lien"><p class="lien-info">Connexion</p></a>
-
-
-
-
-
 </header>
-
-
-
 </body>
-
-
-
-
 </html>
